7b7220ce2da9c10b0b07288409535ccfb7ce21c1
Jaspreet <er.jaspreet.93@gmail.com>
2017-02-19 23:26:11 +0530

    modular approach

--- code.R
+++ code.R
@@ -1,89 +1,76 @@
 # install the basic packages
 install.packages("ROAuth")
 install.packages("twitteR")
-
-# load the respective packages
-library("ROAuth")
-library("twitteR")
-
-# Fetching data from twitter
-consumer_key <-'86HF6yYj3hGwpdGVFOmlnsUeX'
-consumer_secret <-'PYjVFZitzBsrr1wfp1QRzidXVa6OOqEbAET4ri4HBfIu1HIS8Q'
-access_token <-'1111878264-X8NAa8lknRcoVwUcyeiE9RwMAJg6qiJCJ87WOmY'
-access_secret <-'kLd90mJmv2jGYvvRXowVnsz8Sl6p1G7xTZwSUzMLMtWxZ' 
-
-setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
-tweets <- searchTwitter("@airtelindia", n=100, lang = "en")
-# removing retweets
-tweets_noReTweets<- strip_retweets(tweets, strip_manual = TRUE, strip_mt = TRUE)
-#Exploring Tweets.
-length(tweets_noReTweets)
-class(tweets_noReTweets)
-
-#Now fetching the text of these tweets.
-tweets_text <- sapply(tweets_noReTweets, function(x)x$getText())
-#We wil  have all of these as "chracter"
-class(tweets_text)
-
-#Removing unnecessary data
-
-tweets_text <- gsub("@\\w+", "", tweets_text)       # Removing all twitter handle
-tweets_text <- gsub("[[:digit:]]", "", tweets_text) # Removing all the digits
-tweets_text <- gsub("[[:punct:]]", "", tweets_text) # removing All the punctuation
-tweets_text <- gsub("http\\w+", "", tweets_text)    # removing http link
-tweets_text <- sapply(tweets_text, tolower)         # to lower chrachters
-
-# emotion icons pose a challenge in minning tweets , on eplausible solution is filtering tweets on basis of regular expression
-# Here is a function for the same.
-"rmNonAlphabet <- function(str) {
-words <- unlist(strsplit(str, " "))
-in.alphabet <- grep(words, pattern = "[a-z|0-9]", ignore.case = T)
-nice.str <- paste(words[in.alphabet], collapse = " ")
-nice.str
-}"
-
-# trying another approach to remove emoji
-
-tweets_text <- iconv(tweets_text, 'UTF-8', 'ASCII')
-
-"tweets_text <- sapply(tweets_text, rmNonAlphabet)"
-tweets_text <- tweets_text[!is.na(tweets_text)] # removing any missing values if they exist
-names(tweets_text) <- NULL
-
-#have a look at refined Tweets
-head(tweets_text)
-#temp <- "admire admire accurate admire"
-
-# Finally our Data is ready to perform Analysis
-
-"let us convert the twitter tweets_texts into words so that they can be compared and judged"
-# we need stringR package
 install.packages("stringr")
-library(stringr)
-
-words.list <- str_split(tweets_text, " ")   # changed to temp
-
-pos <- scan('positive-words.txt', what = 'character', comment.char = ';')
-neg <- scan('negative-words.txt', what = 'character', comment.char = ';')
-
-words <- unlist(words.list)
-
-pos.matches <- match(words , pos)
-neg.matches <- match(words, neg)
-
-"pos.matches <- !is.na(pos.matches)
-neg.matches <- !is.na(neg.matches)"
-
-score <- sum(pos.matches) - sum(neg.matches)
 
 
-# trying to make histogram of words.
-tbl <- table(pos.matches)
-hdr <- as.integer(names(tbl))
-barplot(rev(tbl), names.arg = pos[hdr], horiz = TRUE)
+"Now lets implement a modular approach"
+
+#load necessary libraries
+load.libraries <- function(){
+  library("ROAuth")
+  library("twitteR")
+  library("stringr")
+}
+
+#function to establish connection with Twitter
+establish.connection <- function(){
+  consumer_key <-'86HF6yYj3hGwpdGVFOmlnsUeX'
+  consumer_secret <-'PYjVFZitzBsrr1wfp1QRzidXVa6OOqEbAET4ri4HBfIu1HIS8Q'
+  access_token <-'1111878264-X8NAa8lknRcoVwUcyeiE9RwMAJg6qiJCJ87WOmY'
+  access_secret <-'kLd90mJmv2jGYvvRXowVnsz8Sl6p1G7xTZwSUzMLMtWxZ' 
+  
+  setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
+}
+
+#function to fetch tweets and prepare them
+fetch.prepare.tweets <- function(keyword,num_tweets){
+  tweets <- searchTwitter(keyword, num_tweets, lang = "en")
+  #preparing tweets
+  tweets_noReTweets<- strip_retweets(tweets, strip_manual = TRUE, strip_mt = TRUE) #eleminating re tweets
+  tweets_text <- sapply(tweets_noReTweets, function(x)x$getText())  # converting to text
+  tweets_text <- gsub("@\\w+", "", tweets_text)       # Removing all twitter handle
+  tweets_text <- gsub("[[:digit:]]", "", tweets_text) # Removing all the digits
+  tweets_text <- gsub("[[:punct:]]", "", tweets_text) # removing All the punctuation
+  tweets_text <- gsub("http\\w+", "", tweets_text)    # removing http link
+  tweets_text <- sapply(tweets_text, tolower)         # to lower chrachters
+  tweets_text <- iconv(tweets_text, 'UTF-8', 'ASCII') # converting to ASCII and hence removing emoji.
+  tweets_text <- tweets_text[!is.na(tweets_text)]     # removing any missing values if they exist
+  names(tweets_text) <- NULL                          # removing the names of Array
+  
+  return(tweets_text)
+}
+
+#function to read necessary files
+read.files <- function(){
+  pos <- scan('positive-words.txt', what = 'character', comment.char = ';')
+  neg <- scan('negative-words.txt', what = 'character', comment.char = ';')
+  
+}
+
+#function analyse positive and negative opinions
+
+  positive.negative.opiniion <- function(refined.tweets, pos, neg){
+    words.list <- str_split(refined.tweets, " ")
+    pos.matches <- match(words , pos)
+    neg.matches <- match(words, neg)
+    
+    total.pos <- sum(table(pos.matches))
+    total.neg <- sum(table(neg.matches))
+    
+    return( (total.pos - total.neg)*100 / (total.pos + total.neg) )
+    
+    
+  }
+  
+  # trying to make histogram of words.
+  tbl <- table(pos.matches)
+  hdr <- as.integer(names(tbl))
+  barplot(rev(tbl), names.arg = pos[hdr], horiz = TRUE)
+  
+  
+  ##################################################################################################
 
 
-##################################################################################################
-"now that we have some logic lets simplify the things by making certain functions"
 
 
