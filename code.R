351509991f2474699da2d3c6e107c9fafc63a674
Jaspreet <er.jaspreet.93@gmail.com>
2017-02-20 23:24:46 +0530

    Added Global variables and barplot

--- code.R
+++ code.R
@@ -28,14 +28,14 @@ fetch.prepare.tweets <- function(keyword,num_tweets){
   tweets <- searchTwitter(keyword, num_tweets, lang = "en")
   #preparing tweets
   tweets_noReTweets<- strip_retweets(tweets, strip_manual = TRUE, strip_mt = TRUE) #eleminating re tweets
-  tweets_text <- sapply(tweets_noReTweets, function(x)x$getText())  # converting to text
-  tweets_text <- gsub("@\\w+", "", tweets_text)       # Removing all twitter handle
-  tweets_text <- gsub("[[:digit:]]", "", tweets_text) # Removing all the digits
-  tweets_text <- gsub("[[:punct:]]", "", tweets_text) # removing All the punctuation
-  tweets_text <- gsub("http\\w+", "", tweets_text)    # removing http link
-  tweets_text <- sapply(tweets_text, tolower)         # to lower chrachters
-  tweets_text <- iconv(tweets_text, 'UTF-8', 'ASCII') # converting to ASCII and hence removing emoji.
-  tweets_text <- tweets_text[!is.na(tweets_text)]     # removing any missing values if they exist
+  tweets_text <<- sapply(tweets_noReTweets, function(x)x$getText())  # converting to text
+  tweets_text <<- gsub("@\\w+", "", tweets_text)       # Removing all twitter handle
+  tweets_text <<- gsub("[[:digit:]]", "", tweets_text) # Removing all the digits
+  tweets_text <<- gsub("[[:punct:]]", "", tweets_text) # removing All the punctuation
+  tweets_text <<- gsub("http\\w+", "", tweets_text)    # removing http link
+  tweets_text <<- sapply(tweets_text, tolower)         # to lower chrachters
+  tweets_text <<- iconv(tweets_text, 'UTF-8', 'ASCII') # converting to ASCII and hence removing emoji.
+  tweets_text <<- tweets_text[!is.na(tweets_text)]     # removing any missing values if they exist
   names(tweets_text) <- NULL                          # removing the names of Array
   
   return(tweets_text)
@@ -43,34 +43,39 @@ fetch.prepare.tweets <- function(keyword,num_tweets){
 
 #function to read necessary files
 read.files <- function(){
-  pos <- scan('positive-words.txt', what = 'character', comment.char = ';')
-  neg <- scan('negative-words.txt', what = 'character', comment.char = ';')
+  pos <<- scan('positive-words.txt', what = 'character', comment.char = ';')
+  neg <<- scan('negative-words.txt', what = 'character', comment.char = ';')
   
 }
 
 #function analyse positive and negative opinions
 
   positive.negative.opiniion <- function(refined.tweets, pos, neg){
-    words.list <- str_split(refined.tweets, " ")
-    pos.matches <- match(words , pos)
-    neg.matches <- match(words, neg)
+    words.list <<- str_split(refined.tweets, " ")
+    w <<- unlist(words.list)
+    pos.matches <<- match(w , pos)
+    neg.matches <<- match(w, neg)
     
-    total.pos <- sum(table(pos.matches))
-    total.neg <- sum(table(neg.matches))
-    
+    total.pos <<- sum(table(pos.matches))
+    total.neg <<- sum(table(neg.matches))
+    barplot(c(total.pos, total.neg), ylim = c(0,500), names.arg = c("Positive" , "Negative"), main = "Opinion Minning")
     return( (total.pos - total.neg)*100 / (total.pos + total.neg) )
     
     
   }
   
+  
+  
+  
+  ##################################################################################################
   # trying to make histogram of words.
   tbl <- table(pos.matches)
   hdr <- as.integer(names(tbl))
   barplot(rev(tbl), names.arg = pos[hdr], horiz = TRUE)
   
+  #Simple barplot of positive and negative opinions
+  barplot(c(total.pos, total.neg), ylim = c(0,500))
   
-  ##################################################################################################
-
 
 
 
